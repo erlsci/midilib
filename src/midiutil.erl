396bf0f82226b894f154da35ed46ef807fbfeb0c
Duncan McGreggor <oubiwann@gmail.com>
2021-01-12 00:06:12 -0600

    Fixed broken util module.

--- src/midiutil.erl
+++ src/midiutil.erl
@@ -4,27 +4,36 @@
 
 seq_to_text(Seq) ->
     seq_to_text(Seq, false).
-seq_to_text(Seq, ShowChanEvents) ->
-    {seq, _, Tracks} = Seq,
+seq_to_text({seq, _, Tracks}, ShowChanEvents) ->
+    seq_to_text({seq, Tracks}, ShowChanEvents);
+seq_to_text({seq, _Header, _MetaTrack, Tracks}, ShowChanEvents) ->
+    seq_to_text({seq, Tracks}, ShowChanEvents);
+seq_to_text({seq, Tracks}, ShowChanEvents) ->
     lists:map(fun(T) -> track_to_text(T, ShowChanEvents) end, Tracks),
     ok.
 
 track_to_text(Track) ->
     track_to_text(Track, false).
 track_to_text(Track, ShowChanEvents) ->
-    io:format("Track start~n"),
+    io:format("~n*** Track start ***~n~n"),
     {track, Events} = Track,
     lists:map(fun(E) -> event_to_text(E, ShowChanEvents) end, Events),
     ok.
 
 event_to_text(Event) ->
     event_to_text(Event, false).
-event_to_text(Event, ShowChanEvents) ->
-    {Name, _} = Event,
-    IsChanEvent = lists:any(fun(X) -> X =:= Name end, 
-			    [off, on, poly_press, controller, program, 
-			     chan_press, pitch_bend]),
+event_to_text({Name, _} = Event, ShowChanEvents) ->
+    event_to_text(Event, Name, ShowChanEvents);
+event_to_text({Name, _, _} = Event, ShowChanEvents) ->
+    event_to_text(Event, Name, ShowChanEvents).
+event_to_text(Event, Name, ShowChanEvents) ->
+    IsChanEvent = lists:member(Name, [off, on, poly_press, controller, program,
+                                      chan_press, pitch_bend]),
     if
-	ShowChanEvents; not IsChanEvent ->
-	    io:format("~p~n", [Event])
+	Name == track_end ->
+            ok;
+        ShowChanEvents; not IsChanEvent ->
+	    io:format("~p~n", [Event]);
+        true ->
+            io:format("~p~n", [Name])
     end.
